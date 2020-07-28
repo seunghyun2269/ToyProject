5b4439b252f774f1426fb7fde16bc1c80dc1db2a
hyunbin1 <kimgusqls1@gmail.com>
2020-07-28 20:18:35 +0900

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kimgu\onedrive\문서\멋쟁이 사자처럼\toyproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
